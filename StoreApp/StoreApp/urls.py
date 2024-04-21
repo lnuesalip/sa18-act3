a7ce3569d9951dfc786b811281a929e43afcead2
Learra Alip <lnuesalip@gmail.com>
2024-04-21 10:14:05 -0500

    Set up a fixture with 3 items

--- StoreApp/StoreApp/urls.py
+++ StoreApp/StoreApp/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('products/', views.product_list, name = 'product_list'),
+    path('products/<int:id>/', views.products_detail, name='product_detail'),
 ]
